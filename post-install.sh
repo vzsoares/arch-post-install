fc3d00dde1b8817916ca19eadc895c5b2be49406
vzsoares <jogolots2@gmail.com>
2024-05-01 13:55:02 -0300

    config

--- post-install.sh
+++ post-install.sh
@@ -1,44 +1,73 @@
 #! /bin/sh
 if ! [ -x "$(command -v yay)" ]; then
-  git clone https://aur.archlinux.org/yay.git
-  cd yay && makepkg -si && cd .. && rm -rf yay
+	git clone https://aur.archlinux.org/yay.git
+	cd yay && makepkg -si && cd .. && rm -rf yay
 fi
 
 if ! [ -x "$(command -v pip3)" ]; then
-  yay -Syu python3-pip
+	yay -Syu python3-pip
 fi
 
-if ! [ -x "$(command -v npm)" ] ||  ! [ -x "$(command -v node)" ]; then
-  yay -Syu nodejs npm
+if ! [ -x "$(command -v npm)" ] || ! [ -x "$(command -v node)" ]; then
+	#yay -Syu nodejs npm
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 fi
 
-export NPM_HOME=$HOME/.npm-global
-export PATH=$PATH:$NPM_HOME/bin
-npm config set prefix $NPM_HOME
+export NPM_HOME="$HOME"/.npm-global
+export PATH="$PATH":"$NPM_HOME"/bin
+npm config set prefix "$NPM_HOME"
 
 echo "Installing system packages..."
 DATA_LOCAL=./pkgs
 for PM in $(ls $DATA_LOCAL); do
-  case $PM in
-  yay) INSTALL_CDM="-Syu --noconfirm" ;;
-  apt) INSTALL_CDM="install -y" ;;
-  pip3) INSTALL_CDM="install --user" ;;
-  npm) INSTALL_CDM="i -g" ;;
-  esac
+	case $PM in
+	yay) INSTALL_CDM="-Syu --noconfirm" ;;
+	apt) INSTALL_CDM="install -y" ;;
+	pip3) INSTALL_CDM="install --user" ;;
+	npm) INSTALL_CDM="i -g" ;;
+	esac
 
-  case $PM in
-  pip3) INSTALL_CDM="sudo $PM $INSTALL_CDM";;
-  *) INSTALL_CDM="$PM $INSTALL_CDM";;
-  esac
+	case $PM in
+	pip3) INSTALL_CDM="sudo $PM $INSTALL_CDM" ;;
+	*) INSTALL_CDM="$PM $INSTALL_CDM" ;;
+	esac
 
-  $INSTALL_CDM $(cat $DATA_LOCAL/$PM)
+	$INSTALL_CDM $(cat $DATA_LOCAL/"$PM")
 done
 
-if [ ! -e 'zshrc' ];
-then
-  echo "Installing zsh configs..."
-  DEST_PATH=../zshrc
-  git clone -q https://github.com/marco-souza/zshrc.git $DEST_PATH
-  cd $DEST_PATH
-  make apply
+if ! command -v zsh &> /dev/null; then
+	echo "Installing zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
+if ! "$(gh auth status)" | grep -q "Active account: true"; then
+	echo "need to auth github and bitbucket"
+	gh auth
+fi
+
+CODE_FOLDER=~/Desktop/code
+
+mkdir $CODE_FOLDER
+
+if ! [ -d "$CODE_FOLDER/scripts" ]; then
+	(cd $CODE_FOLDER && gh repo clone vzsoares/scripts)
+fi
+
+(
+	cd $CODE_FOLDER/scripts || exit
+    cp -r nvim/ ~/.config/
+    cp -r tmux/ ~/.config/
+    cp -r .zshrc ~/
+)
+
+echo "TODO auth chrome"
+echo "TODO auth insomina"
+echo "TODO set zsh default"
+echo "TODO verify nvim"
+echo "TODO auth vscode"
+echo "TODO config vpn"
+echo "TODO auth studio 3t"
+echo "TODO auth mysql workbench"
+echo "TODO auth spotify"
+
+make apply
